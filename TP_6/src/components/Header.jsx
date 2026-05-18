268df0a9729cf254f33fbc089826498726311a15
Valeria Roxana Laguna <valerialaguna0@gmail.com>
2026-05-18 18:53:18 -0300

    modificacion de detalles en Header.jsx

--- TP_6/src/components/Header.jsx
+++ TP_6/src/components/Header.jsx
@@ -3,11 +3,11 @@ function Header() {
     <header>
       <h1>TP6 · React</h1>
       <nav>
-        <a href="#">Cards</a>
-        <a href="#">Contador</a>
-        <a href="#">Formulario</a>
-        <a href="#">Productos</a>
-        <a href="#">To-Do</a>
+        <a href="#cards">Cards</a>
+        <a href="#contador">Contador</a>
+        <a href="#formulario">Formulario</a>
+        <a href="#productos">Productos</a>
+        <a href="#todo">To-Do</a>
       </nav>
     </header>
   )
